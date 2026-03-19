cfc89c32d82af2443ff9a9618345ea54a4c20948
apptesting-dev <jorgeluislimo93@gmail.com>
2026-03-19 00:20:28 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.routes import router
+from app.init_db import init_db
 
 app = FastAPI()
+
+init_db()  # crea tabla automáticamente
 
 app.include_router(router)
 
